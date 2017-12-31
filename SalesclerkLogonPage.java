6d90544ba7aadfb7bc49d00c99eba58fda651291
guijiezheng1119@gmail.com <zhengguijie1119@outlook.com>
2017-12-31 19:00:31 +0800

    update slogon

--- SalesclerkLogonPage.java
+++ SalesclerkLogonPage.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 import javax.swing.event.*;
 import java.util.*;
 
-public class SalesclerkLogonPage extends JFrame implements ActionListener{
+public class SalesclerkLogonPage extends JFrame {
 	JPanel p = new JPanel();
 	JButton b1 = new JButton( "登录" );
 	JButton b2 = new JButton( "重置" );
@@ -15,21 +15,34 @@ public class SalesclerkLogonPage extends JFrame implements ActionListener{
 	JLabel j1 = new JLabel( "请输入用户名" );
 	JTextField jt1 = new JTextField();
 	JLabel j2 = new JLabel( "请输入密码" );
-	JTextField jt2 = new JTextField(); 
+	//JTextField jt2 = new JTextField(); 
+	JPasswordField jpf1 = new JPasswordField(13);
 
 	public SalesclerkLogonPage() {
 		setLayout( new GridLayout( 0, 1 ) );
 		p.setLayout( new FlowLayout() );
 		p.add( b1 );
 		p.add( b2 );
-		b1.addActionListener( this );
-		b2.addActionListener( this );
+		b1.addActionListener( new ActionListener() {
+			public void actionPerformed( ActionEvent e ) {
+				char[] s = jpf1.getPassword();
+				String jpassword = new String(s);
+				System.out.println(jpassword);
+			}
+
+		} );
+		b2.addActionListener( new ActionListener() {
+			public void actionPerformed( ActionEvent e ) {
+				
+			}
+		} );
 
 		p2.setLayout( new GridLayout( 0, 1 ) );
 		p2.add( j1 );
 		p2.add( jt1 );
 		p2.add( j2 );
-		p2.add( jt2 );
+		//p2.add( jt2 );
+		p2.add( jpf1 );
 
 		//getContentPane().add( p2 );
 		//getContentPane().add( p );
@@ -40,22 +53,6 @@ public class SalesclerkLogonPage extends JFrame implements ActionListener{
 		setDefaultCloseOperation( DISPOSE_ON_CLOSE );
 		setLocationRelativeTo( null );
 		setVisible( true);
-	}
-
-	@Override
-	public void actionPerformed( ActionEvent e ) {
-		Object obj = e.getSource();
-		if( obj instanceof JButton ) {
-			JButton srcBtn = (JButton) obj;
-			if( srcBtn.getText().equals( "登陆" ) ) {
-				//在售货员表中查询与售货员相匹配的密码
-			}
-			if( srcBtn.getText().equals( "重置" ) ) {
-				System.out.println( "正在重新输入" );
-				jt1.setText( null );
-				jt2.setText( null );
-			}
-		}	
 	}
 	public static void main(String[] main) {
 		new SalesclerkLogonPage();
