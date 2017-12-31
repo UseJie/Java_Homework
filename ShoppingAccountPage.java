8886cdd24f7b8c8f48de0e112e45721cc20d0eda
guijiezheng1119@gmail.com <zhengguijie1119@outlook.com>
2018-01-01 02:13:56 +0800

    ShoppingAccountPage 还没有完成

--- ShoppingAccountPage.java
+++ ShoppingAccountPage.java
@@ -5,8 +5,11 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.table.*;
+import java.sql.*;
+import java.util.Vector;
+import DB.*;
 
-public class ShoppingAccountPage extends JFrame {
+public class ShoppingAccountPage extends JFrame implements ActionListener{
 	JPanel jp1 = new JPanel();
 	JLabel j1 = new JLabel( "请输入商品名" );
 	JTextField jt1 = new JTextField();
@@ -26,39 +29,78 @@ public class ShoppingAccountPage extends JFrame {
 	JPanel jp5 = new JPanel();
 	JLabel j5 = new JLabel( "找零" );
 	JTextField jt5 = new JTextField();
+	
+	Vector data = new Vector();
+	Vector title = new Vector();
+	DefaultTableModel defaultModel = null;
 
-	Object[][] rowData = {
-							 { "apple", 5, 10 },
-							 { "banana", 2, 20 },
-							};
-	String[] columnNames = { "商品名称", "商品价格", "商品数量" };
-
-	JTable jtb1 = new JTable( rowData, columnNames );
-
+	Connection conn = DbConn.getconn();
+	PreparedStatement pst = null;
+	ResultSet rs = null;
+	String gname;
 	public ShoppingAccountPage() {
 		setLayout( new GridLayout( 0, 1 ) );
 
-		jtb1.setPreferredScrollableViewportSize( new Dimension( 500, 300 ));
-		jtb1.setAutoResizeMode( JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS );
-		TableColumn column = null;
-		for( int i =0; i < 2; i++ ) {
-			column = jtb1.getColumnModel().getColumn(i);
-			if( ( i%2 ) == 0 )
-				column.setPreferredWidth( 150 );
-			else
-				column.setPreferredWidth( 50 );
-		}
-		JScrollPane scrollPane = new JScrollPane( jtb1 );
+		
 		jp1.setLayout( new GridLayout( 0, 1 ) );
 		jp1.add( j1 );
 		jp1.add( jt1 );
-	
-		//jt1.addActionListener();
+		
 
+		JPanel jpTotb = new JPanel( new GridLayout( 0, 1 ) );
+		JTable jtb1 = null;
+		defaultModel = new DefaultTableModel();
+		jtb1 = new JTable( defaultModel );
+		JTableHeader jth1 = jtb1.getTableHeader();
+		title.add( "gid" );
+		title.add( "gname" );
+		title.add( "gprice" );
+		title.add( "gnum" );
+		jpTotb.add( jth1 );
+		jpTotb.add( jtb1 );
+		defaultModel.setDataVector(data, title);
+		
+		jt1.addActionListener( this );
+
+		
 		jp2.setLayout( new GridLayout( 0, 1 ) );
 		jp2.add( j2 );
 		jp2.add( jt2 );
-
+		
+		/*Jt2.addActionListener:
+		* 1. 没有输入商品名/输入名称不对:提示请输入正确商品名
+		* 2. 数量超过库存,重新输入
+		* 3. 更新数据库中的数量
+		*/
+		jt2.addActionListener( new ActionListener() {
+			public void actionPerformed( ActionEvent ae ) {
+				if( data.size() > 0 ) {
+					Integer dataNum = Integer.parseInt(data.get(3).toString());
+					Integer jtNum = Integer.parseInt(jt2.getText());
+					if( dataNum >= jtNum && jtNum >= 0 ) {
+						System.out.println( dataNum + " "+ jtNum );
+						//更新数据中的数量
+					}
+					else {
+						jt2.setText("请输入正确的数量");
+						jt2.addMouseListener( new MouseAdapter() {
+							public void mouseClicked( MouseEvent me ) {
+								jt2.setText(null);
+							}
+						});
+					}
+				}
+				else {
+					jt1.setText("请输入正确的商品名");
+					jt1.addMouseListener( new MouseAdapter() {
+						public void mouseClicked( MouseEvent me ) {
+							jt1.setText(null);
+						}
+					});
+				}
+			}
+		});
+		
 		jp3.setLayout( new GridLayout( 0, 1 ) );
 		jp3.add( j3 );
 		jp3.add( jt3 );
@@ -72,7 +114,7 @@ public class ShoppingAccountPage extends JFrame {
 		jp5.add( jt5 );
 	
 		add( jp1 );
-		add( scrollPane );
+		add( jpTotb );
 		add( jp2 );
 		add( jp3 );
 		add( jp4 );
@@ -85,11 +127,39 @@ public class ShoppingAccountPage extends JFrame {
 		show();
 		setVisible( true );
 		
+		
 		addWindowListener( new WindowAdapter() {
 			public void windowClosing( WindowEvent e ) {
 				System.exit( 0 );
 			}
 		});
+	}
+
+	@Override
+	public void actionPerformed( ActionEvent e ) {
+		gname = jt1.getText().toString();
+		String sql = "SELECT * FROM GOODS WHERE gname=?";
+		try {
+			pst = conn.prepareStatement( sql, Statement.RETURN_GENERATED_KEYS );
+			pst.setString( 1, gname );
+			rs = pst.executeQuery();
+			data.removeAllElements();
+			pack();
+			while( rs.next() ) {
+				Vector line = new Vector();
+				for( int i = 1; i <= 4; i++ ) {
+					line.add( rs.getObject(i) );
+				}
+				data.add( line );
+			} 
+			defaultModel.setDataVector( data, title );
+			pack();
+			DbClose.queryClose( pst, rs, conn );
+		}catch( SQLException sqle ) {
+			sqle.printStackTrace();
+		}finally {
+			DbClose.queryClose( pst, rs, conn );
+		}
 	}
 
 	public static void main(String[] args) {
